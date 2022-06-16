d747f9afaf8f956649ebba9f8146795e0d36b5b4
Cameron Brill <c00brill@gmail.com>
2022-06-15 22:23:44 -0700

    feat: create and use AuthProvider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "@highlight-run/react/dist/highlight.css";
 import type { AppProps } from "next/app";
 import { H } from "highlight.run";
+import { AuthProvider } from "../components/Auth/UserContext";
 import { ErrorBoundary } from "@highlight-run/react";
 import { HighlightOptions } from "../lib/highlight.config";
 
@@ -10,7 +11,9 @@ H.init(process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, HighlightOptions);
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </ErrorBoundary>
   );
 }
