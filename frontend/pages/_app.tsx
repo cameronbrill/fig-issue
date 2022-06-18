aff29d6b358459c566cdba287ff44b64208110e4
Cameron Brill <c00brill@gmail.com>
2022-06-18 11:41:01 -0700

    fix: move non-page config file from pages directory to lib

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,7 +3,7 @@ import "@highlight-run/react/dist/highlight.css";
 import type { AppProps } from "next/app";
 import { H } from "highlight.run";
 import { ErrorBoundary } from "@highlight-run/react";
-import { HighlightOptions } from "./highlight.config";
+import { HighlightOptions } from "../lib/highlight.config";
 
 H.init(process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, HighlightOptions);
 
