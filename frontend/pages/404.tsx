9cdfc255dfce76c320013eb338243947c6138c86
Cameron Brill <c00brill@gmail.com>
2022-06-20 13:52:43 -0700

    feat: make custom 404 page

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,15 @@
+import { Custom404Page } from "../components/Pages/Errors/404";
+
+export default function Custom404() {
+  return (
+    <section
+      style={{
+        height: "85vh",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Custom404Page />
+    </section>
+  );
+}
