cd02cb0db830486c43a5496e006e4999c19e7460
Cameron Brill <c00brill@gmail.com>
2022-06-19 23:17:07 -0700

    feat: remove default Home styles

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
 import { HomePage } from "../components/Pages/Home/Home";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <section className={styles.main}>
+    <section>
       <HomePage />
     </section>
   );
