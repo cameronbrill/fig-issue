694944b8732df35a78b294bada2c6b3a0c22a6d3
Cameron Brill <c00brill@gmail.com>
2022-06-19 23:33:19 -0700

    style: make AuthLogin a section

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -2,7 +2,11 @@ import { NextPage } from "next/types";
 import { Login as AuthLogin } from "../components/Auth/Login";
 
 const Login: NextPage = () => {
-  return <AuthLogin />;
+  return (
+    <section>
+      <AuthLogin />
+    </section>
+  );
 };
 
 export default Login;
