29bd1855c335365e8feefe1a4ce4183f3b9268f3
Cameron Brill <c00brill@gmail.com>
2022-06-19 18:18:30 -0700

    feat: omg so many changes

---
+++ frontend/components/Pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { WelcomeHeroText } from "./WelcomHeroText";
+
+export const HomePage: React.FC = () => {
+  return (
+    <>
+      <WelcomeHeroText />
+    </>
+  );
+};
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,18 +7,46 @@ import { ErrorBoundary } from "@highlight-run/react";
 import { HighlightOptions } from "../lib/highlight.config";
 import { UserProvider } from "@supabase/supabase-auth-helpers/react";
 import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import { useState } from "react";
+import Head from "next/head";
 
 H.init(process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, HighlightOptions);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <ErrorBoundary>
-      <Layout>
-        <UserProvider supabaseClient={supabaseClient}>
-          <Component {...pageProps} />
-        </UserProvider>
-      </Layout>
-    </ErrorBoundary>
+    <>
+      <Head>
+        <title>Fig Issue</title>
+      </Head>
+
+      <ErrorBoundary>
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
+        >
+          <MantineProvider
+            theme={{ colorScheme }}
+            withGlobalStyles
+            withNormalizeCSS
+          >
+            <Layout>
+              <UserProvider supabaseClient={supabaseClient}>
+                <Component {...pageProps} />
+              </UserProvider>
+            </Layout>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </ErrorBoundary>
+    </>
   );
 }
 
