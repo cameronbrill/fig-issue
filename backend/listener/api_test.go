019169887c9221b3e8688829f596997a78abc31f
Cameron Brill <c00brill@gmail.com>
2022-06-21 21:32:58 -0700

    test: re-structure webhook test

--- backend/listener/api_test.go
+++ backend/listener/api_test.go
@@ -11,79 +11,80 @@ import (
 	"github.com/cameronbrill/fig-issue/backend/model/figma"
 )
 
-func TestStart(t *testing.T) {
+func TestWebhook(t *testing.T) {
 	tests := []struct {
-		name        string
-		run         func(t *testing.T, commentChan chan *figma.FigmaFileCommentResponse)
-		commentChan chan *figma.FigmaFileCommentResponse
+		name string
+		body figma.FileCommentResponse
 	}{
 		{
-			name: "test",
-			run: func(t *testing.T, commentChan chan *figma.FigmaFileCommentResponse) {
-				server := Start(context.Background(), commentChan)
-				w := httptest.NewRecorder()
-				reqStr := `{
-  "comment": [
-    {
-      "text": "TODO: \n"
-    },
-    {
-      "mention": "811724164054158337"
-    },
-    {
-      "text": "Change selection colors"
-    },
-    {
-      "text": "!issue"
-    }
-  ],
-  "comment_id": "32",
-  "created_at": "2020-02-23T20:27:16Z",
-  "event_type": "FILE_COMMENT",
-  "file_key": "zH44k2FUM629Fa4EMShiHL",
-  "file_name": "Mockup library",
-  "mentions": [
-    {
-      "id": "811724164054158337",
-      "handle": "Evan Wallace"
-    }
-  ],
-  "order_id": "23",
-  "parent_id": "",
-  "passcode": "secretpasscode",
-  "resolved_at": "",
-  "timestamp": "2020-02-23T20:27:16Z",
-  "triggered_by": {
-    "id": "813845097374535682",
-    "handle": "Dylan Field"
-  },
-  "webhook_id": "22"
- }`
-				var req figma.FigmaFileCommentResponse
-				err := json.Unmarshal([]byte(reqStr), &req)
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				var buf bytes.Buffer
-				err = json.NewEncoder(&buf).Encode(&req)
-				if err != nil {
-					t.Fatal(err)
-				}
-				r, err := http.NewRequest("POST", "/figma", &buf)
-				if err != nil {
-					t.Fatal(err)
-				}
-				r.Header.Set("Content-Type", "application/json")
-				server.Handler.ServeHTTP(w, r)
+			name: "test valid",
+			body: figma.FileCommentResponse{
+				Response: figma.Response{
+					EventType: "FILE_COMMENT",
+					Passcode:  "secretpasscode",
+					Timestamp: "2020-02-23T20:27:16Z",
+					WebhookID: "22",
+				},
+				Comment: []figma.CommentFragment{
+					{
+						Text:    "TODO: \n",
+						Mention: "",
+					},
+					{
+						Text:    "",
+						Mention: "811724164054158337",
+					},
+					{
+						Text:    "Change selection colors",
+						Mention: "",
+					},
+					{
+						Text:    "!issue",
+						Mention: "",
+					},
+				},
+				CommentID: "32",
+				CreatedAt: "2020-02-23T20:27:16Z",
+				FileKey:   "zH44k2FUM629Fa4EMShiHL",
+				FileName:  "Mockup library",
+				Mentions: []figma.User{
+					{
+						Id:     "811724164054158337",
+						Handle: "Evan Wallace",
+						ImgUrl: "",
+						Email:  "",
+					},
+				},
+				OrderID:    "23",
+				ParentID:   "",
+				ResolvedAt: "",
+				TriggeredBy: figma.User{
+					Id:     "813845097374535682",
+					Handle: "Dylan Field",
+					ImgUrl: "",
+					Email:  "",
+				},
 			},
-			commentChan: make(chan *figma.FigmaFileCommentResponse, 8),
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.run(t, tc.commentChan)
+			commentChan := make(chan *figma.FileCommentResponse, 8)
+			server := Start(context.Background(), commentChan)
+			w := httptest.NewRecorder()
+
+			var buf bytes.Buffer
+			err := json.NewEncoder(&buf).Encode(&tc.body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			r, err := http.NewRequest("POST", "/figma", &buf)
+			if err != nil {
+				t.Fatal(err)
+			}
+			r.Header.Set("Content-Type", "application/json")
+			server.Handler.ServeHTTP(w, r)
 		})
 	}
 }
