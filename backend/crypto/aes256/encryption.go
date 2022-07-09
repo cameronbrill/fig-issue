6a1ec75090905768bebce35828c48d1fb9cc245f
Cameron Brill <c00brill@gmail.com>
2022-07-08 22:32:09 -0700

    feat: create aes256 encryption store struct and test

---
+++ backend/crypto/aes256/encryption.go
@@ -0,0 +1,78 @@
+package aes256
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"os"
+	"strings"
+	"unicode"
+)
+
+// credit to author of https://goplay.space/#F_vCnIbq8wY
+
+type EncryptionStore struct {
+	IV         []byte `json:"iv"`
+	CipherText []byte `json:"cipher_text"`
+}
+
+// Encrypt usage:
+// Encrypt(uuid): stored key
+func (e *EncryptionStore) Encrypt(content []byte) (err error) {
+	var encryptionKey []byte = []byte(os.Getenv("AES_ENCRYPTION_KEY"))
+	bPlaintext := _PKCS5Padding(content, aes.BlockSize)
+	block, err := aes.NewCipher(encryptionKey)
+	if err != nil {
+		return err
+	}
+
+	e.IV, _ = generateRandomBytes(block.BlockSize())
+
+	e.CipherText = make([]byte, len(bPlaintext))
+	mode := cipher.NewCBCEncrypter(block, e.IV)
+	mode.CryptBlocks(e.CipherText, bPlaintext)
+
+	return err
+}
+
+func (e *EncryptionStore) Decrypt() (decryptedContent []byte, err error) {
+	var encryptionKey []byte = []byte(os.Getenv("AES_ENCRYPTION_KEY"))
+	block, err := aes.NewCipher(encryptionKey)
+	if err != nil {
+		return nil, err
+	}
+
+	mode := cipher.NewCBCDecrypter(block, e.IV)
+	mode.CryptBlocks(e.CipherText, e.CipherText)
+
+	cutTrailingSpaces := []byte(extraTrim(strings.TrimSpace(string(e.CipherText))))
+	return cutTrailingSpaces, err
+}
+
+func extraTrim(s string) string {
+	clean := strings.Map(func(r rune) rune {
+		if unicode.IsGraphic(r) {
+			return r
+		}
+		return -1
+	}, s)
+	return clean
+}
+
+func generateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func _PKCS5Padding(cipherText []byte, blockSize int) []byte {
+	padding := blockSize - len(cipherText)%blockSize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+
+	return append(cipherText, padText...)
+}
---
+++ backend/crypto/aes256/encryption_test.go
@@ -0,0 +1,75 @@
+package aes256
+
+import (
+	"crypto/aes"
+	"errors"
+	"os"
+	"testing"
+)
+
+func TestEncryption(t *testing.T) {
+	emptyStr := ""
+	tests := []struct {
+		name             string
+		input            string
+		encryptionKey    string
+		hostileEnvAttack *string
+		expectedError    error
+	}{
+		{
+			name:          "test valid",
+			encryptionKey: "1234567890123456",
+			input:         "Hello World",
+		},
+		{
+			name:          "test valid different key and input",
+			encryptionKey: "2234567890123456",
+			input:         "ello World",
+		},
+		{
+			name:          "test valid different key and input longer than 16 characters",
+			encryptionKey: "2234567890123456",
+			input:         "Hello World sixteen characters",
+		},
+		{
+			name:          "test invalid encryption key",
+			encryptionKey: "",
+			input:         "string",
+			expectedError: aes.KeySizeError(0),
+		},
+		{
+			name:             "test hostile take over",
+			encryptionKey:    "2234567890123456",
+			hostileEnvAttack: &emptyStr,
+			input:            "string",
+			expectedError:    aes.KeySizeError(0),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			os.Setenv("AES_ENCRYPTION_KEY", tc.encryptionKey)
+			var encryptionStore EncryptionStore
+			err := encryptionStore.Encrypt([]byte(tc.input))
+			if err != nil {
+				if errors.Is(err, tc.expectedError) {
+					return
+				}
+				t.Fatalf("%+v is not equal to %+v", err, tc.expectedError)
+			}
+			if tc.hostileEnvAttack != nil {
+				os.Setenv("AES_ENCRYPTION_KEY", *tc.hostileEnvAttack)
+			}
+			decryptedValue, err := encryptionStore.Decrypt()
+			if err != nil {
+				if errors.Is(err, tc.expectedError) {
+					return
+				}
+				t.Fatalf("%+v is not equal to %+v", err, tc.expectedError)
+			}
+			if string(decryptedValue) != tc.input {
+				t.Fatalf("decrypted value does not match input: len(%d) %s != len(%d) %s", len(string(decryptedValue)), string(decryptedValue), len(tc.input), tc.input)
+			}
+		})
+	}
+
+}
